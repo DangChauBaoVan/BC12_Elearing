da2b76f0467d2cc1baf8a9d773081a523358930d
Dang Chau Bao Van <dangchaubaovan@gmail.com>
2021-10-14 17:07:17 +0700

    test

--- src/containers/client/Home/Home.jsx
+++ src/containers/client/Home/Home.jsx
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <div>
             <h1 className="text-center">Hello! This is Home</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, eos.</p>
         </div>
     )
 }
